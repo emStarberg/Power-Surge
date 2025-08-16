9d2163b0168b72dff8b647511a8b296520254a2f
Emily Braithwaite <emily@solved.net.nz>
2025-08-16 18:01:32 +1200

    Added camera that follows the player

---
+++ Power Surge/Scenes/Testing/Camera.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public partial class Camera : Camera2D
+{
+	[Export]
+	public NodePath PlayerPath; // Path to the player node
+	private Node2D _player; // Reference to the player node
+
+	public override void _Ready()
+	{
+		Zoom = new Vector2(2.5f,2.5f);
+		_player = GetParent().GetNode<Node2D>("Player");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_player != null)
+		{
+			// Move the camera to follow the player's position
+			Position = _player.Position;
+		}
+	}
+}
