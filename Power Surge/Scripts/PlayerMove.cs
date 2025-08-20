2afe154eaa8d7bece0e972ae4afebc37cc6f3f69
Emily Braithwaite <emily@solved.net.nz>
2025-08-20 23:42:27 +1200

    Player dies after falling off map
    Close #26

--- Power Surge/Scripts/PlayerMove.cs
+++ Power Surge/Scripts/PlayerMove.cs
@@ -10,49 +10,66 @@ public partial class PlayerMove : CharacterBody2D
 	[Export] public float MaxFallSpeed = 1000f; // Terminal velocity
 	[Export] public Node2D _animFolder;
 	private Vector2 velocity;
-	private AnimatedSprite2D IdleAnim, JumpAnim, DeathAnim, HurtAnim;
+	private AnimatedSprite2D IdleAnim, DeathAnim, HurtAnim;
 	private PackedScene JumpAnimation = GD.Load<PackedScene>("Scenes/jump_animation.tscn");
 	private int NumJumps = 0; // For deciding whether a mid air jump is allowed, resets when ground is hit
-
+	private float FallTime = 0f; // For checking if the player has fallen off the map
+	private bool Alive = true;
 	public override void _Ready()
 	{
 		//JumpAnim = _animFolder.GetNode<AnimatedSprite2D>("Anim_Jump");
 		IdleAnim = _animFolder.GetNode<AnimatedSprite2D>("Anim_Idle");
+		DeathAnim = _animFolder.GetNode<AnimatedSprite2D>("Anim_Death");
 		IdleAnim.Play();
 	}
 
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// Apply gravity to Y velocity.
-		velocity.Y += Gravity * (float)delta;
-
-		// Clamp vertical velocity to terminal velocity.
-		velocity.Y = Mathf.Min(velocity.Y, MaxFallSpeed);
-
-		// Get input direction
-		float direction = 0.0f;
-		if (Input.IsActionPressed("input_left"))
-			direction -= 1.0f;
-		if (Input.IsActionPressed("input_right"))
-			direction += 1.0f;
-
-		// Handle horizontal movement
-		velocity.X = direction * Speed;
-
-		// Handle jump
-		if (Input.IsActionJustPressed("input_jump"))
+		if (Alive)
 		{
-			Jump(); // Jump
-		}
+			// Apply gravity to Y velocity.
+			velocity.Y += Gravity * (float)delta;
+			// Clamp vertical velocity to terminal velocity.
+			velocity.Y = Mathf.Min(velocity.Y, MaxFallSpeed);
 
-		// Update velocity
-		Velocity = velocity;
-		// Move
-		MoveAndSlide();
+			// Get input direction
+			float direction = 0.0f;
+			if (Input.IsActionPressed("input_left"))
+				direction -= 1.0f;
+			if (Input.IsActionPressed("input_right"))
+				direction += 1.0f;
+
+			// Handle horizontal movement
+			velocity.X = direction * Speed;
+
+			// Handle jump
+			if (Input.IsActionJustPressed("input_jump"))
+			{
+				Jump(); // Jump
+			}
+
+			// Check how long the player has fallen for
+			if (!IsOnFloor())
+			{
+				FallTime += (float)delta;
+			}
+			else
+				FallTime = 0f;
+
+			if (FallTime > 3f)
+			{
+				Die();
+			}
+
+			// Update velocity
+			Velocity = velocity;
+			// Move
+			MoveAndSlide();
+		}
 	}
 
-	
+
 	/// <Summary>
 	/// Makes the player jump directly upwards with an animation
 	/// </Summary>
@@ -60,7 +77,7 @@ public partial class PlayerMove : CharacterBody2D
 	{
 		if (IsOnFloor() || NumJumps < 2)
 		{
-			// Reset the number of jumps if on floor
+			// Reset number of jumps if on floor
 			if (IsOnFloor())
 			{
 				NumJumps = 0;
@@ -75,5 +92,38 @@ public partial class PlayerMove : CharacterBody2D
 			NumJumps++;
 		}
 
+	}
+
+	/// <Summary>
+	/// Makes player die by playing animation
+	/// </Summary>
+	public void Die()
+	{
+		Alive = false;
+		GD.Print("Dead!");
+		IdleAnim.Stop();
+		IdleAnim.Visible = false;
+		DeathAnim.Visible = true;
+		DeathAnim.Play();
+	}
+
+	/// <Summary>
+	/// Resets level once death animation finished
+	/// </Summary>
+	public void OnDeathAnimFinished()
+	{
+		DeathAnim.Visible = false;
+		// Create timer to wait before reloading
+		Timer timer = new(){
+			WaitTime = 0.6f,
+			OneShot = true
+		};
+		AddChild(timer);
+		timer.Timeout += () => {
+			// Reload scene
+			var scenePath = GetTree().CurrentScene.SceneFilePath;
+			GetTree().ChangeSceneToFile(scenePath);
+		};
+		timer.Start();
 	}
 }
