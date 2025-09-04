9731b612a91eba8d3d7e7df29b9c74af4f877cb3
Emily Braithwaite <emily@solved.net.nz>
2025-09-05 00:15:40 +1200

    Restructured enemies
    Enemies now inherit Enemy.cs base class.
    
    Enemy.cs defines:
    - Hurt()
    - Die()
    - GetHealth()
    - FlashHurtEffect()
    - SwitchAnim()
    
    - isAlive
    - health
    - currentAnimation
    - deathAnim

--- Power Surge/Scripts/Enemies/CircuitBug.cs
+++ Power Surge/Scripts/Enemies/CircuitBug.cs
@@ -2,16 +2,17 @@ using Godot;
 using System;
 //------------------------------------------------------------------------------
 // <summary>
-//   Controls the Circuit Bug enemy
+//   Controls the Circuit Bug enemy.
+// 	 Inherits Enemy
 // </summary>
 // <author>Emily Braithwaite</author>
 //------------------------------------------------------------------------------
-public partial class CircuitBug : CharacterBody2D
+public partial class CircuitBug : Enemy
 {
 	public const float Speed = 50.0f; // Movement speed
 	private Vector2 velocity; // For updating Velocity property
 	private string direction = "left"; // Direction bug is facing
-	private AnimatedSprite2D runAnim, attackAnim, currentAnimation; // Bug animations
+	private AnimatedSprite2D runAnim, attackAnim;
 	private bool isRunning = true; // Whether bug is running
 	private bool playerDetected = false; // Whether player has been detected
 	private PackedScene projectile = GD.Load<PackedScene>("Scenes/projectile_cb.tscn"); // For spawning projectiles
@@ -24,65 +25,73 @@ public partial class CircuitBug : CharacterBody2D
 		wallRay = GetNode<RayCast2D>("WallRay");
 		runAnim = GetNode<AnimatedSprite2D>("Animations/Anim_Run");
 		attackAnim = GetNode<AnimatedSprite2D>("Animations/Anim_Attack");
+		deathAnim = GetNode<AnimatedSprite2D>("Animations/Anim_Death"); // Death animation still to be made
 		currentAnimation = runAnim;
 		currentAnimation.Play();
 
 		attackAnim.FrameChanged += OnAttackAnimFrameChanged;
+
+		health = 10;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// Gravity
-		if (!IsOnFloor())
-			velocity += GetGravity() * (float)delta;
-
-		if (isRunning)
+		if (isAlive)
 		{
-			// Move based on direction
-			if (direction == "right")
-				velocity = new Vector2(Speed, Velocity.Y);
+			
+			// Gravity
+			if (!IsOnFloor())
+				velocity += GetGravity() * (float)delta;
+
+			if (isRunning)
+			{
+				// Move based on direction
+				if (direction == "right")
+					velocity = new Vector2(Speed, Velocity.Y);
+				else
+					velocity = new Vector2(-Speed, Velocity.Y);
+
+				// Check for wall ahead
+				wallRay.ForceRaycastUpdate();
+				if (wallRay.IsColliding())
+				{
+					direction = direction == "right" ? "left" : "right";
+					Scale = new Vector2(-Scale.X, Scale.Y);
+				}
+
+				// Check for ground ahead
+				groundRay.ForceRaycastUpdate();
+				if (!groundRay.IsColliding())
+				{
+					direction = direction == "right" ? "left" : "right";
+					Scale = new Vector2(-Scale.X, Scale.Y);
+				}
+			}
+
+			// Player detection via raycast
+			playerRay.ForceRaycastUpdate();
+			if (playerRay.IsColliding() && playerRay.GetCollider() is Node2D collider && collider.Name == "Player")
+			{
+				if (!playerDetected)
+				{
+					GD.Print("Player detected by raycast");
+					Attack();
+					playerDetected = true;
+				}
+			}
 			else
-				velocity = new Vector2(-Speed, Velocity.Y);
-
-			// Check for wall ahead
-			wallRay.ForceRaycastUpdate();
-			if (wallRay.IsColliding())
 			{
-				direction = direction == "right" ? "left" : "right";
-				Scale = new Vector2(-Scale.X, Scale.Y);
+				if (playerDetected)
+				{
+					GD.Print("Player lost by raycast");
+					playerDetected = false;
+				}
 			}
 
-			// Check for ground ahead
-			groundRay.ForceRaycastUpdate();
-			if (!groundRay.IsColliding())
-			{
-				direction = direction == "right" ? "left" : "right";
-				Scale = new Vector2(-Scale.X, Scale.Y);
-			}
+			Velocity = velocity;
+			MoveAndSlide();
 		}
 
-		// Player detection via raycast
-		playerRay.ForceRaycastUpdate();
-		if (playerRay.IsColliding() && playerRay.GetCollider() is Node2D collider && collider.Name == "Player")
-		{
-			if (!playerDetected)
-			{
-				GD.Print("Player detected by raycast");
-				Attack();
-				playerDetected = true;
-			}
-		}
-		else
-		{
-			if (playerDetected)
-			{
-				GD.Print("Player lost by raycast");
-				playerDetected = false;
-			}
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
 	}
 
 	/// <summary>
@@ -91,7 +100,7 @@ public partial class CircuitBug : CharacterBody2D
 	public void Attack()
 	{
 		GD.Print("attack");
-		SetAnimation(attackAnim);
+		SwitchAnim(attackAnim);
 		runAnim.Visible = false;
 		isRunning = false;
 		// Stop
@@ -106,7 +115,7 @@ public partial class CircuitBug : CharacterBody2D
 		if (!playerDetected)
 		{
 			// Return to running
-			SetAnimation(runAnim);
+			SwitchAnim(runAnim);
 			attackAnim.Visible = false;
 			isRunning = true;
 		}
@@ -116,17 +125,7 @@ public partial class CircuitBug : CharacterBody2D
 			Attack();
 		}
 	}
-	/// <summary>
-	/// Set the current animation plaing
-	/// </summary>
-	/// <param name="anim">Animation to play</param>
-	public void SetAnimation(AnimatedSprite2D anim)
-	{
-		currentAnimation = anim;
-		currentAnimation.Visible = true;
-		currentAnimation.Play();
-	}
-	
+
 	private bool projectileSpawnedThisAttack = false; // Prevents spawning > 1 projectile per animation
 	/// <summary>
 	/// Spawn a projectile on 13th frame to line up with animation
@@ -154,4 +153,14 @@ public partial class CircuitBug : CharacterBody2D
 			projectileSpawnedThisAttack = false;
 		}
 	}
+
+	/// <summary>
+	/// Destroy self once death animation has finished
+	/// </summary>
+	public void OnDeathAnimFinished()
+	{
+		QueueFree();
+	}
+
+
 }
---
+++ Power Surge/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,83 @@
+using Godot;
+using System;
+//------------------------------------------------------------------------------
+// <summary>
+//   Interface to be inherited by all enemies
+// </summary>
+// <author>Emily Braithwaite</author>
+//------------------------------------------------------------------------------
+public abstract partial class Enemy : CharacterBody2D
+{
+	protected bool isAlive = true;
+	protected float health;
+	protected AnimatedSprite2D currentAnimation;
+	protected AnimatedSprite2D deathAnim;
+
+	/// <summary>
+	/// Called when enemy is hit by an attack
+	/// </summary>
+	/// <param name="amount">Amount of health to subtract</param>
+	public void Hurt(float amount)
+	{
+		health -= amount;
+		if (health <= 0)
+		{
+			Die();
+		}
+		else
+		{
+			FlashHurtEffect();
+		}
+
+	}
+	/// <summary>
+	/// Called when health = 0
+	/// </summary>
+	public virtual void Die()
+	{
+		isAlive = false;
+		GD.Print("dead");
+		SwitchAnim(deathAnim);
+
+	}
+
+	/// <summary>
+	/// Get remaining health of enemy
+	/// </summary>
+	/// <returns>Remaining health</returns>
+	public float GetHealth()
+	{
+		return health;
+	}
+	
+	/// <summary>
+	/// Flash with red overlay when hurt
+	/// </summary>
+	protected async void FlashHurtEffect()
+	{
+		if (this.isAlive)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				currentAnimation.Modulate = new Color(1, 0, 0);
+				await ToSignal(GetTree().CreateTimer(0.05f), "timeout");
+				currentAnimation.Modulate = new Color(1, 1, 1);
+				await ToSignal(GetTree().CreateTimer(0.05f), "timeout");
+			}
+		}
+
+	}
+	
+	/// <summary>
+	/// Switch between AnimatedSprite2D
+	/// </summary>
+	/// <param name="to">Animation to switch to</param>
+	protected void SwitchAnim(AnimatedSprite2D to)
+	{
+		currentAnimation.Visible = false;
+		currentAnimation.Stop();
+		currentAnimation = to;
+		//currentAnimation.Visible = true;
+		//currentAnimation.Play();
+	}
+}
