73528fb65d7f8ad49f4be7579c02121b39295e5c
Emily Braithwaite <emily@solved.net.nz>
2025-10-11 02:22:26 +1300

    Added energy deposits
    Worked on level layout

---
+++ Power Surge/Scripts/Objects/EnergyPuddle.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class EnergyPuddle : Area2D
+{
+	private bool playerDetected = false;
+	private float timer = 0;
+	private Player player;
+
+	public override void _Ready()
+	{
+		player = GetParent().GetParent().GetNode<Player>("Player");
+	}
+
+	public override void _Process(double delta)
+	{
+		timer += (float)delta;
+		// Heal every two seconds
+		if (timer >= 2f && playerDetected)
+		{
+			player.IncreasePower(3);
+			timer = 0f;
+		}
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is Player player)
+		{
+			playerDetected = true;
+		}
+	}
+	
+	public void OnBodyExited(Node2D body)
+	{
+		if(body is Player player)
+		{
+			playerDetected = false;
+		}
+	}
+	
+}
